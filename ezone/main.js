fa42b39a87617bf03d713944e63ea86d6436328c
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-11-09 16:33:17 +0100

    added all the buttons and hooked them up with js

--- ezone/main.js
+++ ezone/main.js
@@ -61,7 +61,7 @@ const areas = {
   Injuries: false,
   Technology: false,
   Physiology: false,
-  Psycology: false,
+  Psychology: false,
   Sleep: false,
   Stress: false,
   Tactical: false,
@@ -69,3 +69,110 @@ const areas = {
   Leadership: false,
   Teamwork: false,
 };
+
+let arrayOfGames = [];
+let arrayOfTypes = [];
+let arrayOfAreas = [];
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start");
+  // register toggle-clicks
+
+  document
+    .querySelectorAll(".typeOption")
+    .forEach((selectType) => selectType.addEventListener("click", toggleType));
+  document
+    .querySelectorAll(".gameOption")
+    .forEach((selectGame) => selectGame.addEventListener("click", toggleGame));
+  document
+    .querySelectorAll(".areaOption")
+    .forEach((selectArea) => selectArea.addEventListener("click", toggleArea));
+
+  document.querySelector(".submit").addEventListener("click", pushData);
+}
+
+function toggleType(event) {
+  const target = event.currentTarget;
+  const selectedType = target.dataset.feature;
+
+  if (types[selectedType] === false) {
+    types[selectedType] = true;
+  } else {
+    types[selectedType] = false;
+  }
+
+  if (types[selectedType]) {
+    arrayOfTypes.push(selectedType);
+  } else {
+    const typeIndex = arrayOfTypes.indexOf(selectedType);
+    arrayOfTypes.splice(typeIndex, 1);
+  }
+
+  console.log(arrayOfTypes);
+}
+
+function toggleGame(event) {
+  const target = event.currentTarget;
+  const selectedGame = target.dataset.feature;
+
+  if (games[selectedGame] === false) {
+    games[selectedGame] = true;
+  } else {
+    games[selectedGame] = false;
+  }
+
+  if (games[selectedGame]) {
+    arrayOfGames.push(selectedGame);
+  } else {
+    const gameIndex = arrayOfGames.indexOf(selectedGame);
+    arrayOfGames.splice(gameIndex, 1);
+  }
+
+  console.log(arrayOfGames);
+}
+
+function toggleArea(event) {
+  const target = event.currentTarget;
+  const selectedArea = target.dataset.feature;
+
+  if (areas[selectedArea] === false) {
+    areas[selectedArea] = true;
+  } else {
+    areas[selectedArea] = false;
+  }
+
+  if (areas[selectedArea]) {
+    arrayOfAreas.push(selectedArea);
+  } else {
+    const areaIndex = arrayOfAreas.indexOf(selectedArea);
+    arrayOfAreas.splice(areaIndex, 1);
+  }
+
+  console.log(arrayOfAreas);
+}
+
+function pushData() {
+  const payload = {
+    types: arrayOfTypes,
+    games: arrayOfGames,
+    areas: arrayOfAreas,
+  };
+
+  fetch("https://kea21s-6eb0.restdb.io/rest/experiment", {
+    method: "POST",
+    headers: {
+      "x-apikey": "606d606af55350043100752e",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      console.log(response);
+      arrayOfTypes = [];
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
